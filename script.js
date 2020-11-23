de257a96aeabd3cf6473d3e72ff41f0ec961a675
Martin Šimon <mart.simon17@gmail.com>
2020-11-23 18:29:05 +0000

    Final

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function poNacteni() {
   let dd = x.getDate();
   let mm = x.getMonth();
   let y = x.getFullYear();
-  console.log(dd);  //test proměnných
+  console.log(mm);  //test proměnných
   let vypis = "Dnes je " + Dny_v_tydnu[d] + " " + dd + ". " + Mesice[mm] + " " + y;
   console.log(vypis); // test výpisu
   setInterval(cas, 1000);
@@ -25,4 +25,20 @@ function cas() {
   let time = t.toLocaleTimeString();
   //console.log(time);
   document.getElementById("time").innerHTML = time;  
-}  
+} 
+
+function Patek_13(){
+  let a_mes = new Date().getMonth();
+  let mes = 12;
+  let vypis_p = " "
+  for (let i = 0; i <= mes; i++){
+    let datum = new Date (2020, a_mes + i, 13).getDay();
+    if(datum == 5){
+      let den = new Date (2020, a_mes + i, 13).toLocaleDateString();
+      vypis_p = vypis_p + den + ", ";
+    }
+  }
+  vypis_p += "."
+  document.getElementById("vypis_p").innerHTML = vypis_p;
+  console.log(vypis_p);
+}
